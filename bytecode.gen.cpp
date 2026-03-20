643cf5c6a032db07a32058bca17c7fa73cf5ad4c
rar12455 <rarSS@proton.me>
2026-03-20 16:06:45 +0300

    added bytecode generation skeleton

---
+++ bytecode.gen.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2026 rar <rarSS@proton.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * version 2 any later version.
+ *
+ * This program is distributed in the hope that it will be WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+void
+bytecode_read(auto data_path)
+{
+                while (true)
+                {
+                }
+}
+
+int
+main()
+{
+                std::string file_path = "/home/rar/notes.txt";
+                bytecode_read(file_path);
+                return 0;
+}
