7e168d90c92ebb1e2c2495e93664b0a67978b565
rar12455 <rarSS@proton.me>
2026-03-20 16:30:46 +0300

    added bytecode_read and bytecode_parse functions

--- bytecode.gen.cpp
+++ bytecode.gen.cpp
@@ -15,20 +15,50 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <fstream>
 #include <iostream>
+#include <string>
 
 void
+bytecode_parse(std::string line)
+{
+                for (const auto &i : line)
+                {
+
+                                if (i == ' ')
+                                {
+                                                continue; // ignore whitespace
+                                }
+                                std::cout << "test: " << i << '\n';
+                }
+}
+
+int
 bytecode_read(auto data_path)
 {
-                while (true)
+                std::ifstream file(data_path);
+                std::string   line;
+
+                if (file.is_open())
                 {
+                                while (std::getline(file, line))
+                                {
+                                                bytecode_parse(line);
+                                }
+                                file.close();
                 }
+                else
+                {
+                                std::cerr << "unable to open file, exiting.";
+                                return -1;
+                }
+                return 0;
 }
 
 int
 main()
 {
-                std::string file_path = "/home/rar/notes.txt";
+                auto file_path = "/home/rar/notes.txt";
                 bytecode_read(file_path);
                 return 0;
 }
